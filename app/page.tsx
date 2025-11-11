7b69a3543e1b9ff088f16ae2a3d6326d7340ab0d
ゆむ <ynto.dev@proton.me>
2025-11-11 09:04:15 +0000

    カテゴリにわけた  & 消したりした

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import { ToolCard } from "@/components/ToolCard";
-import tools from "@/data/tools.json";
+import categories from "@/data/tools.json";
 
 export default function Home() {
   return (
-    <div className="px-10 space-y-6 sm:px-20 lg:px-30">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pb-10">
-        {tools.map((tool) => (
-          <ToolCard key={tool.href} {...tool} />
-        ))}
-      </div>
+    <div className="px-10 space-y-10 sm:px-20 lg:px-30 pb-10">
+      {categories.map(({ category, tools }) => (
+        <div key={category}>
+          <h2 className="text-2xl font-bold mb-4 border-b border-gray-300 pb-2">
+            {category}
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {tools.map((tool) => (
+              <ToolCard key={tool.href} {...tool} />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- components/layout/ToolPage.tsx
+++ components/layout/ToolPage.tsx
@@ -1,6 +1,6 @@
 export default function ToolPage({ children }: { children: React.ReactNode }) {
   return (
-    <div className="px-10 space-y-6 sm:px-20 lg:px-30">
+    <div className="px-10 space-y-6 sm:px-20 lg:px-30 pb-10">
       {children}
     </div>
   );
